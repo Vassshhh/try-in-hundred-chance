907cf0b6eb95e86801c5142d4dd3326c51c5ad3a
Mk Ace <mkace667@gmail.com>
2024-10-29 08:56:41 +0000

    ok

--- tracking-app/src/App.js
+++ tracking-app/src/App.js
@@ -1,17 +1,27 @@
-// src/App.js
-import React from 'react';
-import Map from './Map';
+import React, { useState } from "react";
+import Map from "./Map";
+import "./App.css";
 
 const App = () => {
-    return (
-        <div>
-            <h1>Strava Clone</h1>
-            <Map />
+  const [isStarted, setIsStarted] = useState(false);
+
+  const handleStart = () => {
+    setIsStarted(true);
+  };
+
+  return (
+    <div className="app-container">
+      {!isStarted ? (
+        <div className="welcome-screen">
+          <h1>VERRÀT</h1>
+          <button onClick={handleStart}>Start</button>
+          <button> onCLick= window open (https://4qxcwx-8080.csb.app/)</button>
         </div>
-    );
+      ) : (
+        <Map />
+      )}
+    </div>
+  );
 };
 
 export default App;
-
-
-
